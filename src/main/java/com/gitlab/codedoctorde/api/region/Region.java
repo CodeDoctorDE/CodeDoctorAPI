515bf6d87e62186239d701e43a94bb75684260b9
wusel905@gmail.com <wusel905@gmail.com>
2019-10-14 14:23:07 +0200

    update

---
+++ src/main/java/com/gitlab/codedoctorde/api/region/Region.java
@@ -0,0 +1,12 @@
+package com.gitlab.codedoctorde.api.region;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Region {
+    private List<Selection> selections = new ArrayList<>();
+
+    public List<Selection> getSelections() {
+        return selections;
+    }
+}
