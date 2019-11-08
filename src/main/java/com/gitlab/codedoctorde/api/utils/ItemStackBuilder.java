7309999de71ea501a815902265a8bd50b88c5b59
CodeDoctorDE <wusel905@gmail.com>
2019-11-08 20:40:14 +0100

    fix lore

--- src/main/java/com/gitlab/codedoctorde/api/utils/ItemStackBuilder.java
+++ src/main/java/com/gitlab/codedoctorde/api/utils/ItemStackBuilder.java
@@ -165,7 +165,7 @@ public class ItemStackBuilder {
         itemMeta.setDisplayName(MessageFormat.format(itemMeta.getDisplayName(), arguments));
         List<String> formattedLore = new ArrayList<>();
         List<String> lore = itemMeta.getLore();
-        Objects.requireNonNull(lore).stream().map(line -> Arrays.asList(line.split("\n"))).forEach(formattedLore::addAll);
+        Objects.requireNonNull(lore).stream().map(line -> Arrays.asList(MessageFormat.format(line, arguments).split(","))).forEach(lore::addAll);
         Bukkit.broadcastMessage(lore.toString());
         Bukkit.broadcastMessage(formattedLore.toString());
         itemMeta.setLore(formattedLore);
