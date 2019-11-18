7ab84488ae283c0b64b42e5b0e585f8732e96731
CodeDoctorDE <wusel905@gmail.com>
2019-11-18 08:01:26 +0100

    change access from methods in gamestate

--- src/main/java/com/gitlab/codedoctorde/api/game/GameState.java
+++ src/main/java/com/gitlab/codedoctorde/api/game/GameState.java
@@ -3,7 +3,7 @@ package com.gitlab.codedoctorde.api.game;
 import org.bukkit.event.Listener;
 
 public abstract class GameState implements Listener {
-    abstract void start();
+    public abstract void start();
 
-    abstract void stop();
+    public abstract void stop();
 }
