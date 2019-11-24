1deefdf548ee35ce07f7415ff99e7e63bc8c8a79
CodeDoctorDE <wusel905@gmail.com>
2019-11-24 14:33:36 +0100

    change itemstacktypeadapter

--- src/main/java/com/gitlab/codedoctorde/api/serializer/ItemStackTypeAdapter.java
+++ src/main/java/com/gitlab/codedoctorde/api/serializer/ItemStackTypeAdapter.java
@@ -1,49 +1,29 @@
 package com.gitlab.codedoctorde.api.serializer;
 
 import com.google.gson.*;
-import org.bukkit.Material;
+import com.google.gson.reflect.TypeToken;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.io.BukkitObjectInputStream;
-import org.bukkit.util.io.BukkitObjectOutputStream;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Type;
+import java.util.Map;
 
 public class ItemStackTypeAdapter implements JsonSerializer<ItemStack>, JsonDeserializer<ItemStack> {
+    private Gson gson;
 
-    @Override
-    public JsonElement serialize(ItemStack src, Type typeOfSrc, JsonSerializationContext context) {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-            dataOutput.writeObject(src);
-
-            // Serialize that array
-            dataOutput.close();
-            return new JsonPrimitive(Base64Coder.encodeLines(outputStream.toByteArray()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    public ItemStackTypeAdapter() {
+        gson = new GsonBuilder().create();
     }
 
     @Override
-    public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        // Take the JsonElement and create a new Location object given its stored values
-        String value = json.getAsString();
-        if (value != null) try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(value));
-            try (BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream)) {
-                return (ItemStack) dataInput.readObject();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ItemStack(Material.AIR);
-        }
-        else
-            return new ItemStack(Material.AIR);
+    public ItemStack deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context) throws JsonParseException {
+        Map<String, Object> map = gson.fromJson(jsonElement, new TypeToken<Map<String, Object>>() {
+        }.getType());
+        return ItemStack.deserialize(map);
+    }
+
+    @Override
+    public JsonElement serialize(ItemStack itemStack, Type type, JsonSerializationContext context) {
+        return new JsonPrimitive(gson.toJson(itemStack.serialize()));
     }
 
 }
