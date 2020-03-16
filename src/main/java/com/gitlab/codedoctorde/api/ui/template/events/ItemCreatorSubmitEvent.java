751fd88a8febec04fe57881577b907361419ce32
CodeDoctorDE <proficodergames@gmail.com>
2020-03-16 13:07:34 +0100

    add itemcreator

---
+++ src/main/java/com/gitlab/codedoctorde/api/ui/template/events/ItemCreatorSubmitEvent.java
@@ -0,0 +1,9 @@
+package com.gitlab.codedoctorde.itemmods.main;
+
+import org.bukkit.inventory.ItemStack;
+
+public interface ItemCreatorSubmitEvent {
+    default void onEvent(ItemStack itemStack) {
+
+    }
+}
