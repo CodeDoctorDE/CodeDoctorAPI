992cae998cf127a4d00784eeb8327e9318b37953
CodeDoctorDE <proficodergames@gmail.com>
2020-04-06 19:44:09 +0200

    fixing InventoryGui close

--- src/main/java/com/gitlab/codedoctorde/api/ui/inventory/InventoryGui.java
+++ src/main/java/com/gitlab/codedoctorde/api/ui/inventory/InventoryGui.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -81,17 +80,6 @@ public class InventoryGui implements Listener {
 
     public void reload() {
         playerGuiHashMap.forEach((player, gui) -> reload(player));
-    }
-
-
-    @EventHandler
-    public void onInventoryClose(InventoryCloseEvent event) {
-        if (!(event.getPlayer() instanceof Player))
-            return;
-        Player player = (Player) event.getPlayer();
-        if (playerGuiHashMap.containsKey(player))
-            if (playerGuiHashMap.get(player) == this)
-                playerGuiHashMap.get(player).close(player);
     }
 
     @EventHandler
