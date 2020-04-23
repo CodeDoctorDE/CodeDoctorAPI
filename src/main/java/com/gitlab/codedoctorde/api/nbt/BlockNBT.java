17b3e5e449f17345baf34c3b7083dc670a3922d2
CodeDoctorDE <proficodergames@gmail.com>
2020-04-23 14:28:35 +0200

    add get block nbt

---
+++ src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT.java
@@ -0,0 +1,22 @@
+package com.gitlab.codedoctorde.api.nbt;
+
+import com.gitlab.codedoctorde.api.server.Version;
+import org.bukkit.block.Block;
+
+/**
+ * @author CodeDoctorDE
+ */
+public class BlockNBT {
+    public String getNbt(Block block) {
+        switch (Version.getVersion()) {
+            case v1_15:
+                return BlockNBT_v15.getNbt(block);
+            case v1_14:
+                return BlockNBT_v14.getNbt(block);
+            case v1_13:
+                return BlockNBT_v13.getNbt(block);
+            default:
+                return null;
+        }
+    }
+}
---
+++ src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT_v15.java
@@ -0,0 +1,27 @@
+package com.gitlab.codedoctorde.api.nbt;
+
+import com.google.gson.JsonObject;
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+import net.minecraft.server.v1_15_R1.TileEntity;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+
+/**
+ * @author CodeDoctorDE
+ */
+public class BlockNBT_v15 {
+    public static String getNbt(Block block) {
+        CraftWorld ws = (CraftWorld) block.getWorld();
+        NBTTagCompound ntc = null;
+        TileEntity te = (TileEntity) ws.getBlockAt(block.getX(), block.getY(), block.getZ());
+        NBTTagCompound nbtTagCompound = te.persistentDataContainer.toTagCompound();
+        return nbtTagCompound.asString();
+    }
+
+    public static void setNbt(Block block, JsonObject nbt) {
+        CraftWorld ws = (CraftWorld) block.getWorld();
+        NBTTagCompound ntc = null;
+        TileEntity te = (TileEntity) ws.getBlockAt(block.getX(), block.getY(), block.getZ());
+        te.persistentDataContainer.putAll(new NBTTagCompound());
+    }
+}
