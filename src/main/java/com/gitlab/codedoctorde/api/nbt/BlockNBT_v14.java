b371e46ae97a979350d5672cbdce95e500919a8c
CodeDoctorDE <proficodergames@gmail.com>
2020-04-23 14:49:34 +0200

    fix nbt

--- src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT_v14.java
+++ src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT_v14.java
@@ -1,10 +1,13 @@
 package com.gitlab.codedoctorde.api.nbt;
 
 import com.google.gson.JsonObject;
+import net.minecraft.server.v1_14_R1.BlockPosition;
 import net.minecraft.server.v1_14_R1.NBTTagCompound;
 import net.minecraft.server.v1_14_R1.TileEntity;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+
+import java.util.Objects;
 
 /**
  * @author CodeDoctorDE
@@ -13,8 +16,8 @@ public class BlockNBT_v14 {
     public static String getNbt(Block block) {
         CraftWorld ws = (CraftWorld) block.getWorld();
         NBTTagCompound ntc = null;
-        TileEntity te = (TileEntity) ws.getBlockAt(block.getX(), block.getY(), block.getZ());
-        NBTTagCompound nbtTagCompound = te.persistentDataContainer.toTagCompound();
+        TileEntity te = ws.getHandle().getTileEntity(new BlockPosition(block.getX(), block.getY(), block.getZ()));
+        NBTTagCompound nbtTagCompound = Objects.requireNonNull(te).persistentDataContainer.toTagCompound();
         return nbtTagCompound.asString();
     }
 
