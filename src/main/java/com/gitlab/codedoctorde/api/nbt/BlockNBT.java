0275c9a6d6719f51868b7026b429981c8994b3a4
CodeDoctorDE <proficodergames@gmail.com>
2020-04-23 14:29:01 +0200

    bug fixing

--- src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT.java
+++ src/main/java/com/gitlab/codedoctorde/api/nbt/BlockNBT.java
@@ -13,8 +13,6 @@ public class BlockNBT {
                 return BlockNBT_v15.getNbt(block);
             case v1_14:
                 return BlockNBT_v14.getNbt(block);
-            case v1_13:
-                return BlockNBT_v13.getNbt(block);
             default:
                 return null;
         }
