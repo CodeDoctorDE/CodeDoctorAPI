46520b210162b6cefddd38f7193f4b7bd2417f6f
CodeDoctorDE <wusel905@gmail.com>
2021-04-21 14:15:19 +0200

    add controlsBuilder

--- ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/ListGui.java
+++ ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/ListGui.java
@@ -15,18 +15,18 @@ public class ListGui extends GuiCollection {
     private final List<Object> placeholders = new ArrayList<>();
     private final Function<String, GuiItem[]> itemBuilder;
     private final int size;
-    private final GuiPane controls;
+    private final Function<ListGui, GuiPane> controlsBuilder;
     private final Translation translation;
 
-    public ListGui(Translation translation, int size, GuiPane controls, Function<String, GuiItem[]> itemBuilder){
+    public ListGui(Translation translation, int size, Function<ListGui, GuiPane> controlsBuilder, Function<String, GuiItem[]> itemBuilder){
         this.itemBuilder = itemBuilder;
-        this.controls = controls;
+        this.controlsBuilder = controlsBuilder;
         this.size = size;
         this.translation = translation;
         rebuild();
     }
-    public ListGui(Translation translation, GuiPane controls, Function<String, GuiItem[]> itemBuilder){
-        this(translation, 5, controls, itemBuilder);
+    public ListGui(Translation translation, Function<ListGui, GuiPane> controlsBuilder, Function<String, GuiItem[]> itemBuilder){
+        this(translation, 5, controlsBuilder, itemBuilder);
     }
 
     public void setPlaceholders(Object... placeholders){
@@ -43,27 +43,28 @@ public class ListGui extends GuiCollection {
     }
     public void rebuild(String searchText){
         GuiItem[] items = itemBuilder.apply(searchText);
+        GuiPane controls = controlsBuilder.apply(this);
         int controlsCount = controls.getItemCount();
         int freeSlots = size * 9 - controlsCount;
         int pageCount = (int) Math.ceil(items.length / (float) freeSlots);
 
         int currentPage = 1;
 
-        ChestGui currentGui = buildGui(currentPage, pageCount);
+        ChestGui currentGui = buildGui(currentPage, pageCount, controls);
         clearGuis();
         registerGui(currentGui);
 
         for (int i = 0, pageItemCount = 0; i < items.length; i++) {
             if(size * 9 >= pageItemCount + controlsCount) {
                 currentPage++;
-                currentGui = buildGui(currentPage, pageCount);
+                currentGui = buildGui(currentPage, pageCount, controls);
                 registerGui(currentGui);
             }
             currentGui.addItem(items[i]);
             pageItemCount++;
         }
     }
-    private ChestGui buildGui(int currentPage, int pageCount){
+    private ChestGui buildGui(int currentPage, int pageCount, GuiPane controls){
         ChestGui gui = new ChestGui(translation.getTranslation("title", currentPage, pageCount, placeholders));
         gui.addPane(controls);
         return gui;
