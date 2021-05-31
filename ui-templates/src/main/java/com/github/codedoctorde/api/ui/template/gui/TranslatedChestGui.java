95486bbe37801e8f6fd8b5f8db8b4c70b1201575
CodeDoctorDE <wusel905@gmail.com>
2021-05-31 20:14:28 +0200

    rename classes

--- ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/TranslatedChestGui.java
+++ ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/TranslatedChestGui.java
@@ -3,13 +3,13 @@ package com.github.codedoctorde.api.ui.template.gui;
 import com.github.codedoctorde.api.translations.Translation;
 import com.github.codedoctorde.api.ui.ChestGui;
 
-public class TranslationChestGui extends ChestGui {
+public class TranslatedChestGui extends ChestGui {
     private final Translation translation;
 
-    public TranslationChestGui(Translation translation, String title) {
+    public TranslatedChestGui(Translation translation, String title) {
         this(translation, title, 3);
     }
-    public TranslationChestGui(Translation translation, String title, int size) {
+    public TranslatedChestGui(Translation translation, String title, int size) {
         super(title, size);
 
         this.translation = translation;
