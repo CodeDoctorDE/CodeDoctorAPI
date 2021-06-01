4e722cf16c722dcf8df36c76a2f0681af4d9356d
CodeDoctorDE <wusel905@gmail.com>
2021-06-01 13:16:18 +0200

    remove title in TranslatedChestGui

--- ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/TranslatedChestGui.java
+++ ui-templates/src/main/java/com/github/codedoctorde/api/ui/template/gui/TranslatedChestGui.java
@@ -12,11 +12,11 @@ public class TranslatedChestGui extends ChestGui {
     private final Translation translation;
     private List<Object> placeholders = new ArrayList<>();
 
-    public TranslatedChestGui(Translation translation, String title) {
-        this(translation, title, 3);
+    public TranslatedChestGui(Translation translation) {
+        this(translation, 3);
     }
-    public TranslatedChestGui(Translation translation, String title, int size) {
-        super(title, size);
+    public TranslatedChestGui(Translation translation, int size) {
+        super("title", size);
 
         this.translation = translation;
     }
