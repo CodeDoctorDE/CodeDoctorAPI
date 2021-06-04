0ce5674cd1a4c59ac87c5f018045905ad311fb2f
CodeDoctorDE <wusel905@gmail.com>
2021-06-04 14:48:06 +0200

    change gui collection to gui

--- ui/src/main/java/com/github/codedoctorde/api/ui/GuiCollection.java
+++ ui/src/main/java/com/github/codedoctorde/api/ui/GuiCollection.java
@@ -1,19 +1,23 @@
 package com.github.codedoctorde.api.ui;
 
+import com.github.codedoctorde.api.ui.item.GuiItem;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author CodeDoctorDE
  */
-public class GuiCollection {
+public class GuiCollection extends Gui {
     protected final List<Gui> guis = new ArrayList<>();
     private int current;
 
     public GuiCollection() {
-
+        super(0, 0);
     }
 
     public void registerGui(Gui gui) {
@@ -28,20 +32,36 @@ public class GuiCollection {
         guis.clear();
     }
 
+    @Override
     public void show(Player... players) {
         if (hasAccess())
             getGui().show(players);
     }
 
+    @Override
     public void hide(Player... players) {
         if (hasAccess())
             getGui().hide(players);
     }
 
+    @Override
+    public void reload(@NotNull Player... players) {
+        if (hasAccess())
+            getGui().reload(players);
+    }
+
+    @Override
     public Player[] getOpenedPlayers() {
         if (hasAccess())
             return getGui().getOpenedPlayers();
         return new Player[0];
+    }
+
+    @Override
+    public boolean hasCurrentGui(@NotNull Player player) {
+        if (hasAccess())
+            return super.hasCurrentGui(player);
+        return false;
     }
 
     public Gui[] getGuis() {
@@ -58,6 +78,16 @@ public class GuiCollection {
 
     public boolean hasAccess() {
         return current >= 0 && current < guis.size();
+    }
+
+    public void setOpenAction(@Nullable Consumer<Player> openAction) {
+        if (hasAccess())
+            getGui().setOpenAction(openAction);
+    }
+
+    public void setCloseAction(@Nullable Consumer<Player> closeAction) {
+        if (hasAccess())
+            getGui().setCloseAction(closeAction);
     }
 
     public int getCurrent() {
@@ -86,6 +116,28 @@ public class GuiCollection {
         Player[] players = getOpenedPlayers();
         current = guis.size() - 1;
         show(players);
+    }
+
+    @Override
+    public int getHeight() {
+        return getGui().getHeight();
+    }
+
+    @Override
+    public int getWidth() {
+        return getGui().getWidth();
+    }
+
+    @Override
+    public void registerItem(int x, int y, GuiItem item) {
+        if (hasAccess())
+            getGui().registerItem(x, y, item);
+    }
+
+    @Override
+    public void unregisterItem(int x, int y) {
+        if (hasAccess())
+            getGui().unregisterItem(x, y);
     }
 
     public boolean isFirst() {
